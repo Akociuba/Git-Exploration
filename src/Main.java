afe52b8076c7965a9b83ed6c612af0aec5492dd5
AKociuba <127AKociuba@frhsd.com>
2025-09-08 10:55:29 -0400

    Added my original

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hi, my name is Alex.");
-
+        System.out.print("This is my original");
 
         }
     }
