51443b65d40fa2447ac6d0d2639b992918e558db
127JFriedman <127jfriedman@frhsd.com>
2025-09-08 10:36:43 -0400

    Added response

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hi, my name is Alex.");
+        System.out.print("Hi Alex, my name is Jonathan");
 
 
         }
